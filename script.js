192fd09907a2e8def9f0ed00171877268bc22165
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-03 15:20:20 +0530

    using async await

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 console.log('person 1:shows ticket');
 console.log('person 2:shows ticket');
-const promiseWifeBringticks=new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        resolve('ticket');
-    },2000)
-})
-const getpopcorn=promiseWifeBringticks.then((t)=>{
+const premovie = async()=>{
+    const promiseWifeBringingTicks= new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve('ticket');
+        }, 0);
+    });
+    const getpopcorn =new Promise((resolve, reject) => resolve(`popcorn`));
+    const getbutter =new Promise((resolve, reject) => resolve(`butter`));
+    const getcoldrink =new Promise((resolve, reject) => resolve(`coldrink`))
+    let ticket = await promiseWifeBringingTicks;
     console.log('wife: i have ticket');
     console.log('husband: we should go in');
     console.log('wife:no i am hungry');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} popcorn`)
-    })
-}
-)
-const getbutter = getpopcorn.then((t)=>{
+
+    let popcorn = await getpopcorn;
     console.log('husband: i have popcorn');
     console.log('husband: we should go in');
     console.log('wife:no i need butter');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} butter`);
-    })
-})
-const getColddrinks=getbutter.then((t)=>{
+
+    let butter= await getbutter;
     console.log('husband: i have butter');
     console.log('husband: we should go in');
     console.log('wife:no i need coldrinks');
-    return new Promise((resolve, reject) => {
-        resolve(`${t} coldrink`);
-    })
-})
-getColddrinks.then(t=>{
-    console.log(t);
-})
+
+    let coldrinks= await getcoldrink;
+    console.log('husband: i have coldrinks');
+    console.log('wife:Yes lets go');
+
+    return ticket;
+    
+}
+premovie().then((m)=> console.log(`person 3: shows${m}`));
 console.log('person 4:shows ticket');
 console.log('person 5:shows ticket');
